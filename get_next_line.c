796336664a297eaccd6cd49226cde1320c5f6d39
Lucky MHLANGA <lmhlanga@c4r9s4.wethinkcode.co.za>
2019-06-25 16:31:38 +0200

    updates

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,37 @@
 /*   By: lmhlanga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 11:47:24 by lmhlanga          #+#    #+#             */
-/*   Updated: 2019/06/09 12:22:44 by lmhlanga         ###   ########.fr       */
+/*   Updated: 2019/06/25 16:31:24 by lmhlanga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static t_list get_correct_file(t_list **file, int fd)
+{
+	t_list temp;
+
+	temp = *file;
+	while (temp)
+	{
+		if ((int)temp->content_size == fd)
+			return (temp);
+		temp = temp->next;
+	}
+	temp = ft_lstnew('\0', fd);
+	ft_lstadd(file, temp);
+	temp = *file;
+	return (temp);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	char *posLine;
+	char buf[BUFF_SIZE + 1];
+	t_list *curr;
+	static t_list *file;
 
-	posLine = *line;
+	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
+		return (-1);
+	curr = get_correct_file(&file, fd);
+
 }
