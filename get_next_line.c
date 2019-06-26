c1e8ccc68931be29b01bed5e2e3ed55792a19f76
Lucky MHLANGA <lmhlanga@c4r9s4.wethinkcode.co.za>
2019-06-26 15:47:12 +0200

    get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: lmhlanga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 11:47:24 by lmhlanga          #+#    #+#             */
-/*   Updated: 2019/06/25 16:31:24 by lmhlanga         ###   ########.fr       */
+/*   Updated: 2019/06/26 15:26:01 by lmhlanga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static t_list get_correct_file(t_list **file, int fd)
+static t_list		get_correct_file(t_list **file, int fd)
 {
 	t_list temp;
 
@@ -29,14 +29,29 @@ static t_list get_correct_file(t_list **file, int fd)
 	return (temp);
 }
 
-int		get_next_line(const int fd, char **line)
+int					get_next_line(const int fd, char **line)
 {
-	char buf[BUFF_SIZE + 1];
-	t_list *curr;
-	static t_list *file;
+	char			buf[BUFF_SIZE + 1];
+	t_list			*curr;
+	t_list			*ret;
+	static t_list	*file;
 
 	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
 	curr = get_correct_file(&file, fd);
-
+	*line = ft_strnew(1);
+	while (ret = read(fd, buf, BUFF_SIZE))
+	{
+		buf[ret] = '\0';
+		curr->content = ft_strjoin(curr->content, buf);
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	if (ret < BUFF_SIZE && !ft_strlen(curr->content))
+		return (0);
+	i = ft_copyuntil(line, curr->content, '\n');
+	(i < (int)ft_strlen(curr->content))
+		? curr->content += (i + 1)
+		: ft_strclr(curr->content);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,17 @@
 /*   By: lmhlanga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 11:40:04 by lmhlanga          #+#    #+#             */
-/*   Updated: 2019/06/09 11:47:14 by lmhlanga         ###   ########.fr       */
+/*   Updated: 2019/06/26 15:20:58 by lmhlanga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <libft.h>
 
 # define BUFF_SIZE 100
+
+# define MALLCHECK(x) if (!x) return (-1);
 
 int		get_next_line(const int fd, char **line);
 
